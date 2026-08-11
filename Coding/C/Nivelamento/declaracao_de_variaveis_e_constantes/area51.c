db4439b0e834596398ac2451767fc1e77d1fc45b
Pedro Dias Soares <pedro3soares@gmail.com>
2026-08-11 08:49:30 -0300

    Remove unused C files and add a new Hello World example

--- Coding/C/Nivelamento/declaracao_de_variaveis_e_constantes/area51.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int main(){
-    srand(time(NULL));
-    int num_aleatorio = (rand() % (150 - 40)) + 40;
-    printf("Numero aleatorio: %d", num_aleatorio);
-    return 0;
-}
--- Coding/C/Nivelamento/declaracao_de_variaveis_e_constantes/exercicio1.c
+++
@@ -1,8 +0,0 @@
-//Enunciado
-//Declare uma variável do tipo inteiro para armazenar a sua idade, atribua um valor a ela e imprima o valor na tela.
-#include<stdio.h>
-int main(){
-    int idade = 20;
-    printf("Idade = %d", idade);
-    return 0;
-}
