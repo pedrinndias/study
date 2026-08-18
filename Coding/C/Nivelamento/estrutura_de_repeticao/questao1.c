8366a7b7052b498f1a4c6c1333e175653a871b46
Pedro Soares <pedro3soares@gmail.com>
2026-08-18 16:18:12 -0300

    Add documentation for relevant attributes in data analysis survey

---
+++ Coding/C/Nivelamento/estrutura_de_repeticao/questao1.c
@@ -0,0 +1,9 @@
+// Escreva um programa que use um laço for para imprimir os números de 1 a 10, um em cada linha.
+#include<stdio.h>
+int main(){
+    for(int i=0; i<10; i++){
+        printf("%d\n", i+1);
+    }
+
+    return 0;
+}
